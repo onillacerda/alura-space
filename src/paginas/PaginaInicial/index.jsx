d4bd436ab7fe16323a8ca772f410cc28ab2c6d2f
nillacerda <nilson.oliveira@compasso.com.br>
2023-01-20 11:18:10 -0300

    Adicionando componente secao popular

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -4,6 +4,7 @@ import style from "./PaginaInicial.module.scss"
 import Banner from "../../componentes/Banner";
 import Rodape from "../../componentes/Rodape";
 import Galeria from "../../componentes/Galeria";
+import Populares from "../../componentes/Populares";
 
 export default function PaginaInicial(){
     return(
@@ -14,10 +15,11 @@ export default function PaginaInicial(){
                     <Menu />
                     <Banner />
                 </section>
+                <div className={style.galeria}>
+                    <Galeria />
+                    <Populares />
+                </div>
             </main>
-            <div className={style.galeria}>
-                <Galeria />
-            </div>
             <Rodape />
         </>
     );

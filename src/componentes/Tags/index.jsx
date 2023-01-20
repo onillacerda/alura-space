2af0173afa8e23b6f4fab433121bd234c25180e4
nillacerda <nilson.oliveira@compasso.com.br>
2023-01-20 15:02:14 -0300

    adicionando funcionalidades

--- src/componentes/Tags/index.jsx
+++ src/componentes/Tags/index.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import style from './Tags.module.scss';
+import fotos from '../Galeria/fotos.json';
 
-export default function Tags() {
+export default function Tags({tags, filtraFotos, setItens}) {
     return(
         <div className={style.tags}>
             <p className={style.p}>Filtre por tags:</p>
             <ul className={style.tags__lista}>
-                <li>Estrelas</li>
-                <li>Galáxias</li>
-                <li>Lua</li>
-                <li>Planetas</li>
+                {tags.map((tag) =>{
+                    return (
+                        <li key={tag} onClick={()=>filtraFotos(tag)}>
+                            {tag}
+                        </li>)
+                })}
+                <li onClick={()=>setItens(fotos)}>Todas</li>
             </ul>
         </div>
     )
